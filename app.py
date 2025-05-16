7bfde87360775aa8017a78a82e8924de36cd9ba5
Rubiyah Biamin <syaq1603@gmail.com>
2025-05-16 09:54:49 +0000

    2nd commit

--- app.py
+++ app.py
@@ -26,29 +26,4 @@ def index():
 def gemini():
     return render_template("gemini.html")
 
-@app.route("/gemini_reply", methods=["POST"])
-def gemini_reply():
-    q = request.form.get("q")
-    r = model.generate_content(q)
-    return render_template("gemini_reply.html", r=r.text)
 
-# OpenAI routes
-@app.route("/openai", methods=["GET", "POST"])
-def openai():
-    return render_template("openai.html")
-
-@app.route("/openai_reply", methods=["POST"])
-def openai_reply():
-    q = request.form.get("q")
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{"role": "user", "content": q}]
-    )
-    r = response.choices[0].message.content
-
-    markdowner = Markdown()
-    formatted_response = markdowner.convert(r)
-    return render_template("openai_reply.html", r=formatted_response)
-
-if __name__ == "__main__":
-    app.run(debug=True)
